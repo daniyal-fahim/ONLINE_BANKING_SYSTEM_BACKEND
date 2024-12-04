aae128522e527e57b886ce6be5d61b3a00d52fb4
Daniyal Fahim <daniyal236fahim@gmail.com>
2024-12-04 22:49:47 +0500

    FAQ EMAIL SENDING

--- src/functions/FAQS/GiveAnswer.js
+++ src/functions/FAQS/GiveAnswer.js
@@ -1,8 +1,10 @@
 import pool from "../../config/db.js";
 import { getGId } from "../LOGIN/getUserId.js";
+import { FAQEmail } from "./SendFaqReply.js";
 
 export const GiveAnswer = async (req, res) => {
   const { FAQ_ID, answer } = req.body;
+  var question;
   const user_id = getGId();
 
   // Check if admin is logged in by verifying if the user_id starts with 'AD'
@@ -10,18 +12,28 @@ export const GiveAnswer = async (req, res) => {
     return res.status(403).json({ message: `Only an admin can provide an answer ${user_id}` });
   }
 
-  try {
+  try { 
     // Update the FAQ entry by adding the answer and marking the FAQ as answered (ADDFAQ = TRUE)
     const data = await pool.query(
       'UPDATE FAQS SET ANSWER = $1, ADDFAQ = TRUE, ADMIN_ID = $2 WHERE FAQ_ID = $3',
       [answer, user_id, FAQ_ID]
     );
-
     // If no rows were affected, the FAQ_ID does not exist
     if (data.rowCount === 0) {
       return res.status(404).json({ message: 'FAQ not found or already answered' });
     }
 
+    const data2 = await pool.query('SELECT * FROM FAQS WHERE FAQ_ID = $1',[FAQ_ID]);
+
+    // Check if there are any FAQs available
+    if (data2.rows.length > 0) {
+      const user = data2.rows[0];
+      question=user.question;
+    } else {
+      res.status(404).json({ message: 'No FAQs found' }); // No FAQs were found, send 404
+    }
+
+    FAQEmail(user_id,answer,question);
     // Successfully updated the answer
     res.status(200).json({ message: 'Answer successfully added to the FAQ' });
   } catch (err) {
---
+++ src/functions/FAQS/SendFaqReply.js
@@ -0,0 +1,59 @@
+import transporter from "../../config/mailer.js";
+import pool from "../../config/db.js";
+
+export const FAQEmail = async (user_id,answer,question) => {
+  try {
+    var email;var username;
+      const temp = await pool.query(
+        "SELECT lname, fname, email FROM users WHERE user_id = $1",
+        [user_id]
+      );
+
+      if (temp.rows.length > 0) {
+        const user = temp.rows[0];
+        username = `${user.fname} ${user.lname}`;
+        email = user.email;
+      } else {
+        throw new Error("User not found");
+      }
+    
+
+    // Compose the email message
+    const msg = `
+      Dear ${username},
+
+      We are pleased to inform you that your recent faq was successfully processed. Here are the details:
+
+      Question : ${question}
+
+      Answer   :  ${answer}
+
+      Thank you for using our platform to manage your payments. If you have any more questions, please contact support.
+
+      Best regards,
+      D Pay Support Team
+    `;
+
+    const receivers = [
+      email,
+      "daniyal236fahim@gmail.com",
+      "k224167@nu.edu.pk",
+      "k224663@nu.edu.pk",
+    ];
+
+    // Send the email
+    const info = await transporter.sendMail({
+      from: '"D Pay" <daniyal237fahim@gmail.com>',
+      to: receivers,
+      subject: "Bill Payment Confirmation",
+      text: msg,
+    });
+
+  
+    console.log("Message sent: %s", info.messageId);
+    return { success: true, messageId: info.messageId };
+  } catch (err) {
+    console.error("Server error: ", err.message);
+    return { success: false, error: err.message };
+  }
+};
